78b736508bff2a5d64b1f39989c25a0bbe8effab
WillyBarrios <willybarrios@github.com>
2025-12-03 20:06:37 -0600

    Agregar migracion para tabla detalle_pedido

---
+++ database/migrations/2025_12_04_020536_crear_tabla_detalle_pedido.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        // Crear tabla detalle_pedido
+        Schema::create('detalle_pedido', function (Blueprint $table) {
+            // Clave primaria
+            $table->integer('id_detalle_pedido')->autoIncrement()->primary();
+            
+            // Claves foraneas
+            $table->integer('id_pedido')->nullable(false);
+            $table->foreign('id_pedido')->references('id_pedido')->on('pedidos');
+            
+            $table->integer('id_producto')->nullable(false);
+            $table->foreign('id_producto')->references('id_producto')->on('productos');
+            
+            // Campos de datos
+            $table->integer('cantidad')->nullable(false);
+            $table->decimal('precio_unitario', 10, 2)->nullable(false);
+            $table->decimal('subtotal', 10, 2)->nullable(false);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        // Eliminar tabla detalle_pedido
+        Schema::dropIfExists('detalle_pedido');
+    }
+};
