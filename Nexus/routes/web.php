e570d328f1a3480dd60f438ff199c2ff87efe081
Dorian-OR <dorianjosueo@gmail.com>
2025-12-04 18:47:46 -0600

    CAMBIO BORRE URL LOGIN APARTE Y LOGIN FALSO PARA PRUEBA

--- Nexus/routes/web.php
+++ Nexus/routes/web.php
@@ -16,21 +16,12 @@ Route::get('/', function () {
     return view('home');   // usa resources/views/home.blade.php
 })->name('home');
 
-// Login aparte
-Route::get('/login', function () {
-    return view('login');
-})->name('login');
-
 // Login (duplicado, pero lo dejamos igual que estaba)
 Route::get('/login', function () {
     return view('login');
 })->name('login');
 
-Route::post('/login', function () {
-    // Login falso para prueba
-    session(['logged_in' => true]);
-    return redirect('/home');
-});
+
 
 // ruta para olvide mi contraseña (versión simple original)
 Route::get('/password', function () {
@@ -42,13 +33,7 @@ Route::get('/register', function () {
     return view('register');
 });
 
-// 3. Home (protegido)
-Route::get('/home', function () {
-    if (!session('logged_in')) {
-        return redirect('/login');
-    }
-    return view('home');
-})->name('home');
+
 
 // Logout
 Route::post('/logout', function () {
@@ -128,7 +113,7 @@ Route::post('/register', function () {
     return redirect('/home')->with('status', '¡Cuenta creada con éxito! Bienvenido');
 });
 
-
+// categorias
 Route::get('/categorias', function () {
     return view('categories');
 })->name('categories');
